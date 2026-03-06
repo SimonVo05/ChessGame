1f53515427fe59f4f0a075cc931c64e2c9c7bb81
matlua28 <erictran2228@gmail.com>
2026-03-06 15:02:45 -0700

    edit Chess

--- ChessBrowser/Components/ChessGame.cs
+++ ChessBrowser/Components/ChessGame.cs
@@ -4,5 +4,10 @@
     // Represents one instance of a Chess game, such as the players, round, result, moves, event name and date
     public class ChessGame
     {
+        public string EventName { get; set; } = "";
+        public string Site { get; set; } = "";
+        public string Round { get; set; } = "";
+
+
     }
 }
