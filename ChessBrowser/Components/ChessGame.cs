75ff3c675346be93e68abe473d314b26ea961963
matlua28 <erictran2228@gmail.com>
2026-03-06 17:21:44 -0700

    mac ia nen push len

--- ChessBrowser/Components/ChessGame.cs
+++ ChessBrowser/Components/ChessGame.cs
@@ -7,7 +7,15 @@
         public string EventName { get; set; } = "";
         public string Site { get; set; } = "";
         public string Round { get; set; } = "";
+        public string WhitePlayer { get; set } = "";
+        public string BlackPlayer { get; set } = "";
+        public int WhiteElo {  get; set; }
+        public int BlackElo { get;set; }
+        public char Result { get; set; }
+        public string EventDate { get; set; } = "";
+        public string Moves { get; set; } = "";
 
 
+        public ChessGame() { }
     }
 }
---
+++ ChessBrowser/Components/PgnParser .cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace ChessBrowser.Components;
+
+public static class PgnParser
+{
+	public static List<ChessGame> PgnParser(string[] pgnFile)
+	{
+		List<ChessGame> game = new();
+		Dictionary<string, string> tag = new();
+		stringBuilder moves = new();
+		bool moveReading = false; 
+
+        foreach (string pgn in pgnFile.Append(""))
+		{
+			string line = pgn.Trim();
+
+			if (!moveReading)
+			{
+				if (string.IsNullOrEmpty(line))
+				{
+					if (tag.Count > 0)
+					{
+						moveReading = true;
+					}
+				}
+				ParseTag(line, tag);
+
+			}
+			else { 
+				if (string.IsNullOrEmpty(line))
+				{
+					if(tag.Count > 0)
+					{
+
+					}
+				}
+
+			}
+			
+
+		}
+
+	}
+
+	private static void ParseTag(string line, Dictionary<string, string> tag)
+	{
+		if (!line.StartsWith('[') || !line.EndsWith(']'))
+			{ return; }
+		int firstSpace = line.IndexOf(' ');
+		int firstQuote = line.IndexOf('"');
+		int lastQuote = line.LastIndexOf('"');
+
+		if(firstSpace < 0 || firstQuote < 0 || lastQuote < 0 ) 
+			{ return; }
+
+        // For example:  [Event "4. IIFL Wealth Mumbai Op"] 
+        // name : Event , value = "4. IIFL Wealth Mumbai Op"
+
+        string name = line.Substring(1, firstSpace - 1).Trim();
+		string value = line.Substring(firstQuote + 1, lastQuote - firstQuote - 1);
+
+		tag[name] = value;
+	}
+
+
+
+}
